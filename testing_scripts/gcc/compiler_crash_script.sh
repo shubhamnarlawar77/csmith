5c6a2168496e2dccdb6242b76b85171cdb1ca24e
sameeranjoshi <joshisameeran17@gmail.com>
2018-10-01 11:02:33 +0530

    updated compiler crash script

--- testing_scripts/gcc/compiler_crash_script.sh
+++ testing_scripts/gcc/compiler_crash_script.sh
@@ -4,16 +4,18 @@ mkdir stats;
 i=0
 while [ true ]
 do
- ../../src/./csmith --int128 > ../../runtime/int128.c;
- ../../src/./csmith --uint128 > ../../runtime/uint128.c;
- ../../src/./csmith --typeof > ../../runtime/typeof.c;
- ../../src/./csmith --stmt_expr > ../../runtime/stmt_expr.c;
- ../../src/./csmith --computed-goto > ../../runtime/computed_goto.c;
-  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/int128.c &> stats/int128.txt ;
-  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/uint128.c &> stats/uint128.txt;
-  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/typeof.c &> stats/typeof.txt;
-  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/stmt_expr.c &> stats/stmt_expr.txt;
-  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/computed_goto.c &> stats/computed_goto.txt;
+ ../../src/./csmith --computed-goto --typeof > ../../runtime/computed_typeof.c;
+ ../../src/./csmith --uint128 --int128 --computed-goto > ../../runtime/int128_goto.c;
+ ../../src/./csmith --computed-goto --loc-labels > ../../runtime/goto_local_labels.c;
+ ../../src/./csmith --typeof --int128 --uint128 > ../../runtime/typeof_int128.c;
+ ../../src/./csmith --typeof --loc-labels > ../../runtime/typeof_loc_labels.c;
+ 
+  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/computed_typeof.c &> stats/computed_typeof.txt ;
+  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/int128_goto.c &> stats/int128_goto.txt;
+  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/goto_local_labels.c &> stats/goto_local_labels.txt;
+  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/typeof_int128.c &> stats/typeof_int128.txt;
+  gcc -I/home/lab/Downloads/csmith-gcc-extensions/runtime  ../../runtime/typeof_loc_labels.c &> stats/typeof_loc_labels.txt;
+  
   i=$((i+1))
   echo $i&>stats/count.txt
   echo $i
